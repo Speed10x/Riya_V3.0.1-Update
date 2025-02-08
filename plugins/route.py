301ecd799981b5de8111df4590ec434e96a1ed5c
Hemant Kumar <160343394+Speed10x@users.noreply.github.com>
2025-02-08 22:03:30 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -14,13 +14,11 @@ from util.time_format import get_readable_time
 from util.render_template import render_page
 from info import *
 
-
 routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("LucyAutoFilter")
-
+    return web.json_response("BenFilterBot")
 
 @routes.get(r"/watch/{path:\S+}", allow_head=True)
 async def stream_handler(request: web.Request):
